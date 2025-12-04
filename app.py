e8da8014b3ea61bd20bcce6351b0b4818e3d883e
nariman7781 <na0451887@gmail.com>
2025-12-04 12:25:31 +0300

    Add Plotly Express import for data visualization

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 st.set_page_config(page_title="Alzheimer Dataset Viewer", layout="wide")
 st.title("Alzheimer Dataset Interactive Viewer")
